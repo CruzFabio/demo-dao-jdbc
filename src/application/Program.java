a9dde2ea3a7f413f8f07fc74aa95e85cfc6b68ce
CruzFabio <cruz.fabiocorrea@gmail.com>
2026-01-23 19:55:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.Department;
 import model.entities.Seller;
+import model.entities.dao.DaoFactory;
+import model.entities.dao.SellerDao;
 
 import java.util.Date;
 
@@ -14,5 +16,7 @@ public class Program {
 
         Seller obj2 = new Seller(1, "Alex Green", "greenAlex@email.com", new Date(), 5000.0, obj);
         System.out.println(obj2);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
